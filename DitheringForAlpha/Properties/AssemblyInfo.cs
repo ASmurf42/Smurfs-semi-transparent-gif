83ba8001c245b21521cac88dfd839e560ed0e949
Smurfs laptop <hugo.sjo@gmail.com>
2021-05-10 11:37:59 +0200

    Program name in task manager is now correct
    The name in task manager now shows up as "Smurfs Alpha Dithering" instead of DitherForAlpha

--- DitheringForAlpha/Properties/AssemblyInfo.cs
+++ DitheringForAlpha/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DitheringForAlpha")]
+[assembly: AssemblyTitle("Smurfs Alpha Dithering")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DitheringForAlpha")]
+[assembly: AssemblyProduct("Smurfs Alpha Dithering")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
