89c2ee6194834bb3b4db94e8045a9ce5121a1617
Smurfs laptop <hugo.sjo@gmail.com>
2021-05-11 10:53:03 +0200

    hhn

--- DitheringForAlpha/PictureBoxWithInterpolationMode.cs
+++ DitheringForAlpha/PictureBoxWithInterpolationMode.cs
@@ -1,16 +1,16 @@
-﻿using System.Drawing.Drawing2D;
-using System.Windows.Forms;
+﻿//using System.Drawing.Drawing2D;
+//using System.Windows.Forms;
 
-/// <summary>
-/// Inherits from PictureBox; adds Interpolation Mode Setting
-/// </summary>
-public class PictureBoxWithInterpolationMode : PictureBox
-{
-    public InterpolationMode InterpolationMode = (InterpolationMode)5;
+///// <summary>
+///// Inherits from PictureBox; adds Interpolation Mode Setting
+///// </summary>
+//public class PictureBoxWithInterpolationMode : PictureBox
+//{
+//    public InterpolationMode InterpolationMode = (InterpolationMode)5;
 
-    protected override void OnPaint(PaintEventArgs paintEventArgs)
-    {
-        paintEventArgs.Graphics.InterpolationMode = InterpolationMode;
-        base.OnPaint(paintEventArgs);
-    }
-}
+//    protected override void OnPaint(PaintEventArgs paintEventArgs)
+//    {
+//        paintEventArgs.Graphics.InterpolationMode = InterpolationMode;
+//        base.OnPaint(paintEventArgs);
+//    }
+//}
